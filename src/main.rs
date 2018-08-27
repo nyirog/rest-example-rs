5ca9ee6329f42a472b98792722b9bcf360c93503
Nyiro Gergo <gergo.nyiro@gmail.com>
2018-08-27 21:18:35 +0200

    server: rocket + lazy static
    The choice for the server library become rocket. It requires nighly
    build, but since the aim of the project is the client application
    I'm fine with the unsuppoerted features.
    
    The state of the server will be a lazy static HashMap, which can be used
    as a simple lockable module variable.

---
+++ src/main.rs
@@ -0,0 +1,42 @@
+#![feature(plugin)]
+#![plugin(rocket_codegen)]
+
+extern crate rocket;
+extern crate rocket_contrib;
+
+#[macro_use]
+extern crate serde_derive;
+#[macro_use]
+extern crate lazy_static;
+
+use rocket_contrib::Json;
+use std::collections::HashMap;
+use std::sync::Mutex;
+
+#[derive(Serialize, Deserialize, Clone)]
+pub struct User {
+    pub name: String,
+}
+
+lazy_static! {
+    static ref USERS: Mutex<HashMap<u32, User>> = {
+        let mut m = HashMap::new();
+        m.insert(
+            0,
+            User {
+                name: "admin".to_string(),
+            },
+        );
+        Mutex::new(m)
+    };
+}
+
+#[get("/")]
+fn list() -> Json<HashMap<u32, User>> {
+    let users = USERS.lock().unwrap();
+    Json((*users).clone())
+}
+
+fn main() {
+    rocket::ignite().mount("/users", routes![list]).launch();
+}
