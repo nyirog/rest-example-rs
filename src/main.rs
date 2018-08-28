7297bd3e9855b849cd315ef574009886755ad88f
Nyiro Gergo <gergo.nyiro@gmail.com>
2018-08-28 22:34:28 +0200

    server: error handling
    Replace the naive implementation with proper error hanling.
    The unwrap calls can be replaced with "?" (try! macro earlier)
    so the return type became Result<..., ErrorType> which caused
    the map_err calls to convert the errors to the same type.
    
    This error propagation bubbles up without losing the cause of the error,
    and the sugar syntax of "?" makes it elegant (early return without extra
    indentaion).

--- src/main.rs
+++ src/main.rs
@@ -7,6 +7,8 @@ extern crate rocket_contrib;
 #[macro_use]
 extern crate serde_derive;
 
+use rocket::http::Status;
+use rocket::response::status;
 use rocket::State;
 use rocket_contrib::Json;
 use std::collections::HashMap;
@@ -20,20 +22,32 @@ pub struct User {
 type Users = HashMap<u32, User>;
 
 #[get("/")]
-fn list(users: State<Mutex<Users>>) -> Json<HashMap<u32, User>> {
-    Json((*users.inner()).lock().unwrap().clone())
+fn list(common_users: State<Mutex<Users>>) -> Result<Json<Users>, status::Custom<String>> {
+    let users = (*common_users.inner()).lock().map_err(|_| {
+        status::Custom(
+            Status::InternalServerError,
+            format!("Resource is locked by other user\n"),
+        )
+    })?;
+    Ok(Json(users.clone()))
 }
 
 #[get("/<user_id>")]
-fn get(users: State<Mutex<Users>>, user_id: u32) -> Json<User> {
-    Json(
-        (*users.inner())
-            .lock()
-            .unwrap()
-            .get(&user_id)
-            .unwrap()
-            .clone(),
-    )
+fn get(
+    common_users: State<Mutex<Users>>,
+    user_id: u32,
+) -> Result<Json<User>, status::Custom<String>> {
+    let users = (*common_users.inner()).lock().map_err(|_| {
+        status::Custom(
+            Status::InternalServerError,
+            format!("Resource is locked by other user\n"),
+        )
+    })?;
+    let user = users.get(&user_id).ok_or(status::Custom(
+        Status::NotFound,
+        format!("Invalid user id: {}\n", user_id),
+    ))?;
+    Ok(Json(user.clone()))
 }
 
 fn init_users() -> Mutex<Users> {
