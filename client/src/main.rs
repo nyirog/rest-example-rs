74e5d9ce567f8d2f05b4c9cd730d026df7f94176
Nyiro Gergo <gergo.nyiro@gmail.com>
2018-09-10 22:59:42 +0200

    client: use reqwest as http library
    The first step of the client is to fetch data from the server.
    
    It is not so easy to find a stable http request library whithout aysnc
    mechism. Most of the simple libraries don't live long. The more stable
    libraries are async (tokio) heavy. Hyper seems a de facto choice for
    http and reqwest is a simple wrapper over hyper which fades the future
    handling from the user.
    
    At a final step maybe the hyper library should be used directly.
    
    However the json parsing went easy after a little reading. Since the
    serde_json::Value can be used as parsing type, and it gives the required
    flexibility for ruther manipulation.

--- client/src/main.rs
+++ client/src/main.rs
@@ -1,3 +1,15 @@
+extern crate reqwest;
+extern crate serde_json;
+
+use serde_json::value::Value;
+
+fn fetch() -> std::result::Result<Value, reqwest::Error> {
+    let response: Value = reqwest::get("http://localhost:8000/users/0")?.json()?;
+    Ok(response)
+}
+
 fn main() {
     println!("Hello, world!");
+    let user = fetch().unwrap();
+    println!("{}", user);
 }
