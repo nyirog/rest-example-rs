d2bb7cd57e5a27da1bdd3efe0b3201423bfa4f7e
Nyiro Gergo <gergo.nyiro@gmail.com>
2018-09-10 23:26:02 +0200

    client: REPL
    A simple recursieve call was enough to implement the interactive session
    of the user.

--- client/src/main.rs
+++ client/src/main.rs
@@ -2,14 +2,29 @@ extern crate reqwest;
 extern crate serde_json;
 
 use serde_json::value::Value;
+use std::io::{self, BufRead};
 
-fn fetch() -> std::result::Result<Value, reqwest::Error> {
-    let response: Value = reqwest::get("http://localhost:8000/users/0")?.json()?;
+
+fn fetch(url: String) -> std::result::Result<Value, reqwest::Error> {
+    let response: Value = reqwest::get(format!("http://localhost:8000/{}", url).as_str())?.json()?;
     Ok(response)
 }
 
-fn main() {
-    println!("Hello, world!");
-    let user = fetch().unwrap();
+fn do_loop() -> Result<(), std::io::Error>{
+    let mut line = String::new();
+    let stdin = io::stdin();
+    stdin.lock().read_line(&mut line)?;
+
+    if line.len() == 0 {
+        return Ok(())
+    }
+
+    let user = fetch(line).unwrap();
     println!("{}", user);
+    do_loop()
+}
+
+fn main() -> Result<(), std::io::Error>{
+    do_loop()?;
+    Ok(())
 }
