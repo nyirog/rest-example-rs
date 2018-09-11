bb2bf8e30eb92f87c93fca9725ca7288992cd2ec
Nyiro Gergo <gergo.nyiro@gmail.com>
2018-09-11 08:26:47 +0200

    client: command parsing
    Command parsing had to be introduced before new user commands can be
    implemented. split_whitespace and a match statement do the job fair
    enough. It's not a sophisticated one, but our usecase is quite simple.
    The rust infrastructure encourages simple solution.
    
    I had a look at nom library as a lexer, but the examples were more
    complicated to achive a simple goal like string matching. It may worst
    to try it later.

--- client/src/main.rs
+++ client/src/main.rs
@@ -1,30 +1,37 @@
-extern crate reqwest;
+#[macro_use]
 extern crate serde_json;
 
-use serde_json::value::Value;
-use std::io::{self, BufRead};
+extern crate reqwest;
 
+use serde_json::value::Value;
+use std::io::{self, BufRead, Error, ErrorKind};
 
 fn fetch(url: String) -> std::result::Result<Value, reqwest::Error> {
-    let response: Value = reqwest::get(format!("http://localhost:8000/{}", url).as_str())?.json()?;
+    let response: Value =
+        reqwest::get(format!("http://localhost:8000/{}", url).as_str())?.json()?;
     Ok(response)
 }
 
-fn do_loop() -> Result<(), std::io::Error>{
+fn do_loop() -> Result<(), Error> {
     let mut line = String::new();
     let stdin = io::stdin();
     stdin.lock().read_line(&mut line)?;
+    let command: Vec<&str> = line.split_whitespace().collect();
 
-    if line.len() == 0 {
-        return Ok(())
+    if command.len() == 0 {
+        return Ok(());
     }
 
-    let user = fetch(line).unwrap();
+    let user = match command[0] {
+        "get" => fetch(command[1].to_string()),
+        _ => Ok(json!(null)),
+    }.map_err(|e| Error::new(ErrorKind::Other, format!("{}", e)))?;
+
     println!("{}", user);
     do_loop()
 }
 
-fn main() -> Result<(), std::io::Error>{
+fn main() -> Result<(), std::io::Error> {
     do_loop()?;
     Ok(())
 }
